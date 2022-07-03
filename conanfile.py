9ee8a395f6db26f42bc5b437fc58bd08b1e7dd5a
Alexander Bigerl <alexander@bigerl.eu>
2022-07-03 14:34:32 +0200

    updated to conan center recipe

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ import os
 from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 from conans.tools import rmdir
+from conan.tools.microsoft import is_msvc
 
 required_conan_version = ">=1.33.0"
 
@@ -31,26 +32,33 @@ class Recipe(ConanFile):
         "fPIC": True
     }
     license = "ISC"
-    exports_sources = "src*", "include*", "doc*", "waf", "wscript", "waflib*", "serd.pc.in"
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
 
     def configure(self):
-        # its a C library. So C++ properties compiler.{libcxx,cppstd} are not required.
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
+        if self.options.shared:
+            del self.options.fPIC
 
     def validate(self):
-        if self.settings.compiler == "Visual Studio":
+        if tools.cross_building(self):
+            raise ConanInvalidConfiguration("Cross compiling is not supported by serd's build system Waf.")
+
+        if is_msvc(self):
             raise ConanInvalidConfiguration("Don't know how to setup WAF for VS.")
 
     def build(self):
         args = ["--no-utils", " --prefix={}".format(self.folders.package_folder)]
-        if not self.options["shared"]:
+        if not self.options.shared:
             args += ["--static", "--no-shared"]
-        if self.options["fPIC"]:
-            args += ["-fPIC"]
         args = " ".join(arg for arg in args)
 
         cflags = []
+        if self.options.get_safe("fPIC"):
+            cflags += ["-fPIC"]
         if self.settings.build_type in ["Debug", "RelWithDebInfo"]:
             cflags += ["-g"]
         if self.settings.build_type in ["Release", "RelWithDebInfo"]:
@@ -61,16 +69,23 @@ class Recipe(ConanFile):
             cflags += ["-Os"]
         cflags = " ".join(cflag for cflag in cflags)
 
-        self.run(f'CFLAGS="{cflags}" ./waf configure {args}')
-        self.run('./waf build')
+        self.run(f'CFLAGS="{cflags}" ./waf configure {args}', run_environment=True)
+        self.run('./waf build', run_environment=True)
 
     def package(self):
-        self.run('./waf install')
+        self.run('./waf install', run_environment=True)
         rmdir(os.path.join(self.package_folder, "share"))
-        rmdir(os.path.join(self.package_folder, "lib/pkgconfig"))
-        self.copy("COPYING", src=self.folders.base_export_sources, dst="licenses")
+        rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        self.copy("COPYING", src=self.folders.base_source, dst="licenses")
 
     def package_info(self):
-        libname = "{}-0".format(self.name)
+        libname = f"{self.name}-0"
         self.cpp_info.libs = [libname]
-        self.cpp_info.includedirs = [f"include/{libname}/"]
+        self.cpp_info.includedirs = [os.path.join("include", libname)]
+        self.cpp_info.set_property("pkg_config_name", libname)
+
+        # TODO: to remove in conan v2 once pkg_config generators removed
+        self.cpp_info.names["pkg_config"] = libname
+
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.append("m")
